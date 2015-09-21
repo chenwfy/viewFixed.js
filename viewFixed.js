b0d9ba02654dcf4333ff56faa364aaf8bb7e671a
chenwfy <chenwfy@163.com>
2015-09-21 15:14:53 +0800

    Create viewFixed.js

---
+++ viewFixed.js
@@ -0,0 +1,31 @@
+(function (win) {
+    var ua = win.navigator.userAgent,
+        isWP = -1 != ua.indexOf("Windows Phone"),
+        isIPod = /iPod/.test(ua) && !isWP,
+        isIPad = /iPad/.test(ua) && !isWP,
+        isIPhone = /iPhone/.test(ua) && !isWP,
+        isAndroid = -1 != ua.indexOf('Android') && !isWP,
+        isDroidPhone = isAndroid && -1 != ua.indexOf('Mobile'),
+        isDroidPad = isAndroid && !isDroidPhone,
+        isMobile = isWP || isDroidPhone || isDroidPad || isIPhone || isIPod || isIPad,
+        baseWidth = (isIPad || isDroidPad) ? 720.0 : 480.0,
+        initViewport = 'initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, width=device-width',
+        docEl = document.documentElement,
+        metaEl = docEl.querySelector('meta[name="viewport"]'),
+        resetViewport = function () {
+            var time = Number(arguments[0]) || 0;
+            time > 0 && metaEl.setAttribute('content', initViewport);
+            var setViewport = function () {
+                var sWidth = docEl.getBoundingClientRect().width, scale = sWidth / baseWidth;
+                metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no, width=' + baseWidth);
+            };
+            (time == 0 && setViewport()) || (time > 0 && win.setTimeout(setViewport, time));
+        };
+
+    if (isMobile) {
+        resetViewport();
+        win.onorientationchange = function () { resetViewport(200); };
+    }
+
+    win.Device = { WindowsPhone: isWP, AndroidPhone: isDroidPhone, AndroidPad: isDroidPad, IPhone: isIPhone, IPod: isIPod, IPad: isIPad };
+})(window);
