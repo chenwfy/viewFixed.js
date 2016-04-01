ed15365ffc976ba73a5871c70fdc041f35b5b9de
chenwfy <chenwfy@163.com>
2016-04-01 17:33:57 +0800

    更新
    新增运行环境检测等

--- viewFixed.js
+++ viewFixed.js
@@ -1,44 +1,42 @@
 (function (win) {
+    'use strict';
+
+    var docEl = win.document.documentElement,
+        viewportEl = docEl.querySelector('meta[name="viewport"]');
+
+    if (!!!viewportEl) throw 'the tag: <head><meta name="viewport"></head> not found!';
+
     var ua = win.navigator.userAgent,
-        inWeixin = -1 != ua.indexOf("MicroMessenger"),
-        isUCWeb = -1 != ua.indexOf("UCBrowser"),
-        isWP = -1 != ua.indexOf("Windows Phone"),
-        isIPod = /iPod/.test(ua) && !isWP,
-        isIPad = /iPad/.test(ua) && !isWP,
-        isIPhone = /iPhone/.test(ua) && !isWP,
-        isAndroid = -1 != ua.indexOf('Android') && !isWP,
-        isDroidPhone = isAndroid && -1 != ua.indexOf('Mobile'),
-        isDroidPad = isAndroid && !isDroidPhone,
-        isMobile = isWP || isDroidPhone || isDroidPad || isIPhone || isIPod || isIPad,
-        baseWidth = (isIPad || isDroidPad) ? 720.0 : 480.0,
-        initViewport = 'initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, width=device-width',
-        docEl = document.documentElement,
-        metaEl = docEl.querySelector('meta[name="viewport"]'),
+        isH5Plus = -1 != ua.indexOf("Html5Plus"),                   //HBuilder的webview
+        isWeixin = -1 != ua.indexOf("MicroMessenger"),              //微信
+        isUCWeb = -1 != ua.indexOf("UCBrowser"),                    //UCWeb浏览器
+        isWindowsPhone = -1 != ua.indexOf("Windows Phone"),
+        isIPod = /iPod/.test(ua) && !isWindowsPhone,
+        isIPad = /iPad/.test(ua) && !isWindowsPhone,
+        isIPhone = /iPhone/.test(ua) && !isWindowsPhone,
+        isAndroid = -1 != ua.indexOf('Android') && !isWindowsPhone,
+        isAndroidPhone = isAndroid && -1 != ua.indexOf('Mobile'),
+        isAndroidPad = isAndroid && !isAndroidPhone,
+        isMobile = isWindowsPhone || isAndroidPhone || isAndroidPad || isIPhone || isIPod || isIPad,
+        baseWidth = (isIPad || isAndroidPad) ? 720.0 : 480.0,
+        densitydpi = isAndroid && !inWeixin && !isUCWeb ? ', target-densitydpi=device-dpi' : '',
+        baseViewportContent = 'initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, width=device-width' + densitydpi,
         resetViewport = function () {
-            var time = Number(arguments[0]) || 0;
-            time > 0 && metaEl.setAttribute('content', initViewport);
-            var setViewport = function () {
-                var sWidth = docEl.getBoundingClientRect().width,
-                    scale = sWidth / baseWidth,
-                    setViewport = 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no, width=' + baseWidth;
+            var time = ~~arguments[0],
+                setViewport = function () {
+                    var sWidth = docEl.getBoundingClientRect().width,
+                        scale = sWidth / baseWidth,
+                        newViewportContent = 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no, width=' + baseWidth + densitydpi;
 
-                if (isAndroid && !inWeixin && !isUCWeb) {
-                    setViewport += ', target-densitydpi=device-dpi';
-                }
+                    viewportEl.setAttribute('content', newViewportContent);
+                };
 
-                metaEl.setAttribute('content', setViewport);
-            };
-            (time == 0 && setViewport()) || (time > 0 && win.setTimeout(setViewport, time));
+            (time == 0 && setViewport()) || (time > 0 && viewportEl.setAttribute('content', baseViewportContent) && win.setTimeout(setViewport, time));
         };
 
-    if (isAndroid && !inWeixin && !isUCWeb) {
-        initViewport += ', target-densitydpi=device-dpi';
-    }
-
-    if (isMobile) {
-        resetViewport();
-        win.onorientationchange = function () { resetViewport(200); };
-    }
-
-    win.Device = { WindowsPhone: isWP, AndroidPhone: isDroidPhone, AndroidPad: isDroidPad, IPhone: isIPhone, IPod: isIPod, IPad: isIPad };
+    isMobile
+    && resetViewport()
+    && (win.onorientationchange = function () { resetViewport(200); })
+    && (win.Device = { WindowsPhone: isWindowsPhone, AndroidPhone: isAndroidPhone, AndroidPad: isAndroidPad, IPhone: isIPhone, IPod: isIPod, IPad: isIPad })
+    && (win.Runtime = { Weixin: isWeixin, Html5Plus: isH5Plus, UCWeb: isUCWeb });    //这里后续需要继续完善不同的运行环境，如：微博内置浏览器、QQ内置浏览器、系统原生浏览器等
 })(window);
